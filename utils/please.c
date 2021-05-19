ed2bbed73bbf6b1b4ee685e6d9d099d180bff96e
Pierre Gimalac <pierre.gimalac@gmail.com>
2021-05-19 15:41:06 +0200

    add utils, please

---
+++ utils/please.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int, char *argv[]) {
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0) {
+        execlp("notify-send", "-u", "normal", "password needed", NULL);
+        perror("execlp");
+    } else {
+        argv[0] = "sudo";
+        execvp(argv[0], argv);
+        perror("execvp");
+    }
+
+    return 0;
+}
