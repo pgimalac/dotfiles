8c1091a47745069c04c3f387c0070b2e2ddfeb3e
Pierre Gimalac <pierre.gimalac@gmail.com>
2021-05-27 23:20:58 +0200

    update polybar config to get logs

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,10 +4,10 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+while pgrep -u $UID -x polybar >/dev/null; do
+    sleep 0.1;
+done
 
 # Launch bar
-polybar -r top &
-polybar -r bot;
-
-echo "Bars launched..."
+pdetach polybar -r top 2>>/tmp/polybar_top.log 1>&2
+pdetach polybar -r bot 2>>/tmp/polybar_bot.log 1>&2
